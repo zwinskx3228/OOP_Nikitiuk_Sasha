ce1d20e5c7af9bf78f629d84b559109e9db78e56
Sasha <sasha342008@gmail.com>
2025-11-23 22:06:30 +0200

    update

---
+++ Csharp/LW6/lr3 z2/Hotel.cs
@@ -0,0 +1,43 @@
+﻿using static lr_1_3.Form1;
+
+namespace lr_1_3
+{
+    public class Hotel
+    {
+        public string Name { get; set; }
+        public List<Form1.Room> Rooms { get; set; } = new();
+
+        // Делегати / Events
+        public event Action<Form1.Room>? RoomAdded;
+        public event Action<Form1.Room>? RoomUpdated;
+        public event Action<Form1.Room>? RoomDeleted;
+        public event Action<string>? StatusMessage;
+
+        public Hotel(string name)
+        {
+            Name = name;
+        }
+
+        public void AddRoom(Form1.Room room)
+        {
+            Rooms.Add(room);
+            RoomAdded?.Invoke(room);
+            StatusMessage?.Invoke($"Додано кімнату: {room.RoomType}, {room.price} грн");
+        }
+
+        public void RemoveRoom(Form1.Room room)
+        {
+            Rooms.Remove(room);
+            RoomDeleted?.Invoke(room);
+            StatusMessage?.Invoke($"Видалено: {room.RoomType}");
+        }
+
+        // Сортування за допомогою Comparison<T>
+        public List<Form1.Room> SortByPrice()
+        {
+            List<Form1.Room> sorted = Rooms.OrderBy(r => r.price).ToList();
+            StatusMessage?.Invoke("Виконано сортування за ціною");
+            return sorted;
+        }
+    }
+}
---
+++ Csharp/LW6/lr3 z2/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr_1_3
+{
+    public class Logger
+    {
+        public event EventHandler<string>? LogUpdated;
+        public List<string> Logs { get; } = new();
+
+        public void Log(string message)
+        {
+            string entry = $"[{DateTime.Now:T}] {message}";
+            Logs.Add(entry);
+            LogUpdated?.Invoke(this, entry);
+        }
+    }
+}
